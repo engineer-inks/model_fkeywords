4f0961ba06529cca0f287ce6fa988b19ffcd32f0
Eneas <eneas.rodrigues25@gmail.com>
2022-01-12 19:32:25 -0300

    fix ajustes no setup do projeto

--- setup.py
+++ setup.py
@@ -7,21 +7,20 @@ import os
 
 PROJECT_DIR = os.path.dirname(__file__)
 DEPENDENCIES = open(os.path.join(PROJECT_DIR, 'requirements.txt')).readlines()
+required_libs = ['api']
 
 setup(
-   
     name='api',
     version='0.1.0',
     description='A Natural Language Processing Library',
     author='Eneas Rodrigues',
     license='MIT',
-    packages=find_packages(include=['api.*']),
+    packages=find_packages(include=required_libs),
     install_requires=[d for d in DEPENDENCIES if '://' not in d],
     python_requires='>=3.7',
     #TO-DO: Fix dependency links : not working with bdist_wheel
     dependency_links = ["git+https://github.com/explosion/spacy-models/releases/download/pt_core_news_sm-3.2.0/pt_core_news_sm-3.2.0.tar.gz"],
     tests_require=['pytest', 'parameterized'],
     zip_safe=False
-    
 )
 
