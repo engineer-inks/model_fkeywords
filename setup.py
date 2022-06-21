502c6dddb2d60a9202e63454164171c3e3f7084a
Eneas <eneas.rodrigues25@gmail.com>
2022-06-20 22:56:32 -0300

    fix - setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 from setuptools.command.install import install
 from setuptools import setup, find_packages
 from distutils.command.install import install as _install
 
 import os
 
+
 PROJECT_DIR = os.path.dirname(__file__)
+INFO = open(os.path.join(PROJECT_DIR, 'INFO')).readlines()
+INFO = dict((line.strip().split('=') for line in INFO))
+
 DEPENDENCIES = open(os.path.join(PROJECT_DIR, 'requirements.txt')).readlines()
 
 setup(
     name='model_fkeywords',
-    version='0.1.2',
     description='A Natural Language Processing Library',
-    author='Eneas Rodrigues',
-    license='MIT',
-    packages=find_packages(include=['api_model']),
+    version=INFO['version'],
+    author=INFO['author'],
+    author_email=INFO['author_email'],
+    url=INFO['url'],    
+    license=open(os.path.join(PROJECT_DIR, 'LICENSE')).read(),
+    packages=find_namespace_packages(include=['api_model','api_model.utils']),
     install_requires=[d for d in DEPENDENCIES if '://' not in d],
     python_requires='==3.7.13',
     #TO-DO: Fix dependency links : not working with bdist_wheel
